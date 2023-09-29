54c5efd9b61cdc2999f0c9fcc61ced0b53a0120f
Anton Markov <amrkov5@gmail.com>
2023-09-29 16:54:03 +0300

    feat: added bird jump

--- random-game/script.js
+++ random-game/script.js
@@ -18,8 +18,6 @@ function setCanvasSize() {
     }
 }
 
-
-
 bg.src = './assets/sprites/background-day.png';
 pipeTop.src = './assets/sprites/pipe-green-top.png';
 pipeBot.src = './assets/sprites/pipe-green-bot.png';
@@ -30,15 +28,47 @@ let pipeY = 0;
 let birdY = 235;
 let pipesX = 250;
 const speed = 1;
-const birdFall = 3;
+const birdFall = 2;
+let baseArr = [];
 const pipe = [{x: 250, y: 0}]
-const baseArr = [0, base.width - 22];
 
 function randomYPipe() {
     min = -250;
     max = 550;
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function addRemovePipes() {
+    if (pipe[0].x == Math.floor(canvas.width * 0.3)) {
+        pipe.push({x:canvas.width, y: 0})
+    }
+    if (pipe[0].x === 0 - pipeBot.width){
+        pipe.shift()
+    }
+}
+
+let isUp = false;
+let birdJump = 0;
+function flap() {
+    // console.log(birdY - Math.floor(canvas.height * 0.13), Math.floor(canvas.height * 0.13), birdY)
+    if (birdY >= birdJump) {
+        birdY -= 7;
+    } else {
+        isUp = false;
+    }
+    // if (i >= Math.floor(canvas.height * 0.13)) {
+    // }
+}
+
+function addRemoveBase() {
+    if (baseArr[baseArr.length - 1] + base.width === canvas.width ) {
+        baseArr.push(canvas.width);
+    }
+    if (baseArr[0] + base.width === 0) {
+        baseArr.shift();
+    }
+}
+
 function draw() {
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
     for (let i = 0; i < pipe.length; i++) {
@@ -46,28 +76,28 @@ function draw() {
         ctx.drawImage(pipeBot, pipe[i].x, pipeTop.height + pipe[i].y + 130);
         pipe[i].x--;
     }
-    for (let i = 0; i < 2; i++){
+    for (let i = 0; i < baseArr.length; i++) {
         ctx.drawImage(base, baseArr[i], canvas.height - base.height);
         baseArr[i]--;
     }
-    ctx.drawImage(bird, 15, birdY);
-    birdY += birdFall;
-    if (pipe[0].x == Math.floor(canvas.width * 0.3)) {
-        pipe.push({x:canvas.width, y: 0})
-        // console.log(pipe[0])
+    if (!isUp) {
+        birdY += birdFall;
+    } else {
+        flap()
     }
-    if (pipe[0].x === 0 - pipeBot.width){
-        pipe.shift()
-    }
-    console.log(baseArr)
-    if (baseArr[0] + base.width - 20 === 0 ) {
-        baseArr[0] = baseArr[1] + base.width - 22;
-    }
-    if (baseArr[1] + base.width - 22 === 0 ) {
-        baseArr[1] = baseArr[0] + base.width - 22;
-    }
+    ctx.drawImage(bird, canvas.width * 0.08, birdY);
+    addRemovePipes()
+    addRemoveBase()
     requestAnimationFrame(draw);
 }
 
-base.onload = draw;
+base.onload = function () {
+    baseArr = [0, base.width];
+    draw();
+}
+document.addEventListener('click', () => {
+    birdJump = birdY - Math.floor(canvas.height * 0.1);
+    isUp = true;
+});
+// document.addEventListener('keydown', flap);
 document.addEventListener('DOMContentLoaded', setCanvasSize);
