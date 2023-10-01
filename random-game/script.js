c3835b7c1f2a682424c10def9b99662bf8c6fc7b
Anton Markov <amrkov5@gmail.com>
2023-10-01 19:39:23 +0300

    feat: added game count, added leaderboard, added gameover screen

--- random-game/script.js
+++ random-game/script.js
@@ -13,22 +13,9 @@ const wing = new Audio();
 const hit = new Audio();
 const startBtn = new Image();
 const leaderBrdBtn = new Image();
-
-function setCanvasWidth() {
-    const width = window.innerWidth * 0.3;
-    if (width > 550) {
-        return 550;
-    }
-    return width;
-}
-
-function setCanvasHeight() {
-    const height = window.innerHeight * 0.8;
-    if (height > 768) {
-        return 768;
-    } 
-    return height;
-}
+const returnBtn = new Image();
+const gameOverImg = new Image();
+const countDigit = new Image();
 
 //load assets
 bg.src = './assets/sprites/background-day.png';
@@ -39,28 +26,63 @@ base.src = './assets/sprites/base.png';
 wing.src = './assets/audio/wing.ogg';
 hit.src = './assets/audio/hit.ogg';
 startBtn.src = './assets/sprites/multimedia.png';
-leaderBrdBtn.src = './assets/sprites/podium.png'
+leaderBrdBtn.src = './assets/sprites/podium.png';
+returnBtn.src = './assets/sprites/turn-back.png';
+gameOverImg.src = './assets/sprites/gameover.png';
 
 //set variables
-const birdX = canvas.width * 0.08
+const birdX = canvas.width * 0.08;
 const speed = 1;
 const defaultSpeed = 2;
 const gravity = 0.1;
-const pipe = [{x:canvas.width,y: randomYPipe()}]
+const pipe = [{x:canvas.width,y: randomYPipe()}];
 const gap = Math.floor(canvas.height * 0.25);
+const leaderboardW = canvas.width - canvas.width * 0.2;
+const leaderboardH = canvas.height - canvas.height * 0.2;
+// const leaderboardArr = [];
+const btnsX = Math.floor(canvas.width / 2) - 50;
+const startBtnY = canvas.height * 0.15 ;
+const leaderBrdBtnY = canvas.height - (canvas.height * 0.15) - 100;
+const returnBtnY = canvas.width * 0.1 + leaderboardH - 50;
 let birdFallSpeed = 2;
 let baseArr = [];
 let pipesX = 0;
 let animationFrameId;
 let pipeY = 0;
-let birdY = 235;
+let birdY = canvas.height / 2;
 let isPlaying = false;
 let isInMenu = true;
 let isInLeaderboard = false;
-let btnsX = Math.floor(canvas.width / 2)- 50;
-let startBtnY = canvas.height * 0.15 ;
-let leaderBrdBtnY = canvas.height -  (canvas.height * 0.15) - 100;
+let scoreGap = leaderboardH * 0.03;
+let scoreOffset = scoreGap;
+let gameOver = false;
+let gameCount = 0;
+let isShowing = false;
+let curTime = 0;
+let count = [0];
 
+//set canvas size
+function setCanvasWidth() {
+    const width = window.innerWidth * 0.3;
+    if (width > 550) {
+        return 550;
+    }
+    if (width < 230) {
+        return 230;
+    }
+    return width;
+}
+
+function setCanvasHeight() {
+    const height = window.innerHeight * 0.8;
+    if (height > 768) {
+        return 768;
+    } 
+    if ( height < 340) {
+        return 340;
+    }
+    return height;
+}
 
 //get random for pipes Y position
 function randomYPipe() {
@@ -109,25 +131,26 @@ function addRemoveBase() {
 }
 
 //collisions
-function isTouchCollision() {
+function hasTouchedCollision() {
     pipeTopCoordinate = pipe[0].y + pipeTop.height;
     if (birdY <= 0) {
         hit.play();
-        cancelAnimationFrame(animationFrameId);
+        gameOver = true;
+        isPlaying = false;
     }
     if (birdY + bird.height > canvas.height - base.height) {
         hit.play();
-        cancelAnimationFrame(animationFrameId)
+        gameOver = true;
         isPlaying = false;
     }
     if (birdY <= pipeTopCoordinate && pipe[0].x < (birdX + bird.width - 2) && (pipe[0].x + pipeTop.width) >= birdX) {
         hit.play();
-        cancelAnimationFrame(animationFrameId);
+        gameOver = true;
         isPlaying = false;
     }
     if (birdY + bird.height >= pipeTopCoordinate + gap && pipe[0].x < (birdX + bird.width - 2) && (pipe[0].x + pipeTop.width) >= birdX) {
         hit.play();
-        cancelAnimationFrame(animationFrameId);
+        gameOver = true;
         isPlaying = false;
     }
 }
@@ -141,6 +164,13 @@ function eventHandler() {
         wing.play();
         isUp = true;
     }
+    if (gameOver) {
+        location.reload()
+    }
+    if (isInMenu && event.key === " ") {
+        isInMenu = false;
+        isPlaying = true;
+    }
 }
 
 //Start game and open leaderboard
@@ -149,40 +179,113 @@ function clickBtns() {
     const canvasX = canvas.getBoundingClientRect().left;
     pointerY = event.clientY;
     pointerX = event.clientX;
-    if (!isPlaying) {
+    if (!isPlaying && isInMenu || !isPlaying && isInLeaderboard) {
         if (pointerX >= canvasX + btnsX && pointerX <= canvasX + btnsX + 100
             && pointerY >= canvasY + startBtnY && pointerY <= canvasY + startBtnY + 100) {
             isPlaying = true;
             isInMenu = false;
         }
         if (pointerX >= canvasX + btnsX && pointerX <= canvasX + btnsX + 100
-            && pointerY >= canvasY + leaderBrdBtnY && pointerY <= canvasY + leaderBrdBtnY + 100) {
+            && pointerY >= canvasY + leaderBrdBtnY && pointerY <= canvasY + leaderBrdBtnY + 100 && !isInLeaderboard) {
             isInMenu = false;
             isInLeaderboard = true;
+        } else if (pointerX >= canvasX + btnsX + 25 && pointerX <= canvasX + btnsX + 75
+                && pointerY >= canvasY + returnBtnY && pointerY <= canvasY + returnBtnY + 50 && isInLeaderboard) {
+                isInMenu = true;
+                isInLeaderboard = false;
         }
     }
 }
 
+//drawing leaderboard menu
+function drawLeaderboard() {
+    const leaderboardArr = saveGetResults('get');
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = "#000";
+    ctx.fillRect(canvas.width * 0.1, canvas.width * 0.1, leaderboardW, leaderboardH - 70);
+    ctx.globalAlpha = 1;
+    for (let i = 0; i < leaderboardArr.length; i++) {
+        ctx.globalAlpha = 0.4;
+        ctx.fillStyle = "#000";
+        ctx.fillRect(canvas.width * 0.1 + scoreGap, canvas.width * 0.1 + scoreOffset, leaderboardW - scoreGap * 2, (leaderboardH - scoreGap * 11 - 70) / 10);
+        ctx.globalAlpha = 1;
+        ctx.fillStyle = '#fff'
+        ctx.font = `${((leaderboardH - scoreGap * 11 - 70) / 10) - 1}px Arial`
+        const text = 'Score: ' + leaderboardArr[i];
+        const textX = (canvas.width * 0.1 + scoreGap) + 5;
+        const textY = canvas.width * 0.1 + scoreOffset + 2 + scoreGap;
+        if (leaderboardArr[i]) {
+            ctx.fillText(text, textX, textY);
+        }
+        scoreOffset += scoreGap + (leaderboardH - scoreGap * 11 - 70) / 10;
+    }
+    scoreOffset = scoreGap;
+    ctx.drawImage(returnBtn, btnsX + 25, returnBtnY, 50, 50);
+}
+
+//countScore
+function countScore(timestamp) {
+    if (pipe[0].x + pipeTop.width === Math.floor(birdX)) {
+        gameCount += 1;
+        isShowing = true;
+        curTime = timestamp;
+        count = gameCount.toString().split().map(Number)
+    }
+}
+
+//draw game count
+function drawCount() {
+    for (let i = 0; i < count.length; i++) {
+        countDigit.src = `./assets/sprites/${count[i]}.png`;
+        if (!gameOver) {
+            ctx.drawImage(countDigit, canvas.width / 2 - (countDigit.width / 2) * count.length, canvas.height * 0.25);
+        } else {
+            ctx.drawImage(countDigit, canvas.width / 2 - (countDigit.width / 2) * count.length, canvas.height * 0.45 + gameOverImg.height);
+        }
+    }
+}
+
+//save results
+function saveGetResults(task) {
+    const savedResult = JSON.parse(localStorage.getItem('leaderboard'));
+    if (task = 'get') {
+        if (!savedResult) {
+            return [];
+        }
+        return JSON.parse(localStorage.getItem('leaderboard'));
+    }
+    if (task = 'set') {
+        if (!savedResult) {
+            localStorage.setItem('leaderboard', JSON.stringify([gameCount]));
+        }
+
+    }
+}
+
 //drawing the game
-function draw() {
+function draw(timestamp) {
+    // console.log(timestamp)
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
-    // console.log(birdY)
-    if (isPlaying) {
-        for (let i = 0; i < pipe.length; i++) {
-            ctx.drawImage(pipeTop, pipe[i].x, pipe[i].y);
-            ctx.drawImage(pipeBot, pipe[i].x, pipeTop.height + pipe[i].y + gap);
+    for (let i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeTop, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBot, pipe[i].x, pipeTop.height + pipe[i].y + gap);
+        if (isPlaying) {
             pipe[i].x--;
         }
+    }
+    if (isPlaying) {
         if (!isUp) {
             birdY += birdFall();
         } else {
             flap()
         }
-        addRemovePipes();
     }
+    addRemovePipes();
     for (let i = 0; i < baseArr.length; i++) {
         ctx.drawImage(base, baseArr[i], canvas.height - base.height);
-        baseArr[i]--;
+        if (!gameOver) {
+            baseArr[i]--;
+        }
     }
     addRemoveBase();
     ctx.drawImage(bird, birdX, birdY);
@@ -191,14 +294,18 @@ function draw() {
         ctx.drawImage(leaderBrdBtn, btnsX, leaderBrdBtnY, 100, 100);
     }
     if (isInLeaderboard) {
-        ctx.globalAlpha = 0.5;
-        ctx.fillStyle = 'gray'
-        ctx.fillRect(canvas.height * 0.1, canvas.width * 0.1, canvas.width - canvas.width * 0.2,canvas.height - canvas.heigth * 0.1)
-        ctx.globalAlpha = 1;
-
+        drawLeaderboard();
+    }
+    if (gameOver) {
+        ctx.drawImage(gameOverImg, btnsX + 50 - gameOverImg.width / 2, canvas.height * 0.35);
+        drawCount()
+    }
+    countScore(timestamp);
+    if (isShowing && timestamp < curTime + 1500) {
+        drawCount()
     }
     animationFrameId = requestAnimationFrame(draw);
-    isTouchCollision()
+    hasTouchedCollision()
 }
 
 base.onload = function () {
@@ -207,5 +314,9 @@ base.onload = function () {
 }
 canvas.addEventListener('mousedown', clickBtns);
 document.addEventListener('click', eventHandler);
-document.addEventListener('keydown', eventHandler);
-window.addEventListener('blur', cancelAnimationFrame(animationFrameId));
+document.addEventListener('keydown', () =>{
+    if (event.key === " ") {
+        eventHandler()
+    }
+});
+// window.addEventListener('blur', cancelAnimationFrame(animationFrameId));
